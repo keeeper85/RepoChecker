cb33de4223aa4d78a6a85ecb7bdc286d9877a482
keeeper85 <122378010+keeeper85@users.noreply.github.com>
2024-07-24 11:00:44 +0200

    Initial commit

---
+++ src/main/java/eu/wswieciejutra/repo_checker/RepoCheckerApplication.java
@@ -0,0 +1,13 @@
+package eu.wswieciejutra.repo_checker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepoCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepoCheckerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/wswieciejutra/repo_checker/RepoCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.wswieciejutra.repo_checker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepoCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
