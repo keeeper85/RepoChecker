fef53992e8673f662fc4bfb6b0eca4f5c99eabed
keeeper85 <122378010+keeeper85@users.noreply.github.com>
2024-07-31 20:12:07 +0200

    Now can set caching on/off in app properties

--- src/main/java/eu/wswieciejutra/repo_checker/service/Facade.java
+++ src/main/java/eu/wswieciejutra/repo_checker/service/Facade.java
@@ -3,20 +3,27 @@ package eu.wswieciejutra.repo_checker.service;
 import eu.wswieciejutra.repo_checker.exception.UserNotFoundException;
 import eu.wswieciejutra.repo_checker.service.dto.RepositoryDto;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-@AllArgsConstructor
 public class Facade {
 
     private final Factory factory;
     private final SearchResultCachingService searchResultCachingService;
+    private final boolean caching;
+
+    public Facade(Factory factory, SearchResultCachingService searchResultCachingService, @Value("${repo-checker.caching}") boolean caching) {
+        this.factory = factory;
+        this.searchResultCachingService = searchResultCachingService;
+        this.caching = caching;
+    }
 
     public List<RepositoryDto> getNonForkRepositories(String service, String username, String token) throws UserNotFoundException {
 
-        if (searchResultCachingService.hasRepositoryBeenCached(username)) {
+        if (caching && searchResultCachingService.hasRepositoryBeenCached(username)) {
             return searchResultCachingService.getCachedRepositories(username)
                     .stream()
                     .map(repo -> Factory.convertCachedRepositoryToDto(repo, searchResultCachingService.getCachedBranches(repo)))
@@ -25,7 +32,7 @@ public class Facade {
 
         Services serviceType = Services.valueOf(service.toUpperCase());
         List<RepositoryDto> repositories = factory.getService(serviceType).getNonForkRepositories(username, token);
-        searchResultCachingService.cacheRepositories(repositories);
+        if(caching) searchResultCachingService.cacheRepositories(repositories);
         return repositories;
     }
 }
